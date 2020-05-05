ec92b60a5ee6aea7e4272cae79793879eb3dd3bd
ChauPham90 <minhchaupham90@gmail.com>
2020-05-05 16:50:02 +0700

    solution for rentals

--- advance-express/vidly/index.js
+++ advance-express/vidly/index.js
@@ -1,12 +1,16 @@
 const Joi = require("joi");
 const express = require("express");
 const app = express();
-const genres = require("./routes/genres");
+//const movies = require("./routes/movies");
+const rental = require("./routes/rental");
+//const genres = require("./routes/genres");
 //const customers = require("./routes/customers");
 
 app.use(express.json());
-app.use("/api/genres", genres);
+//app.use("/api/genres", genres);
 //app.use("/api/customers", customers);
+//app.use("/api/movies", movies);
+app.use("/api/rental", rental);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
