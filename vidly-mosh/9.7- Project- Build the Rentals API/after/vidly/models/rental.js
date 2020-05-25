861d5e8b5d99a347d5506ece1639091b2cc0a808
ChauPham90 <minhchaupham90@gmail.com>
2020-05-25 11:57:56 +0700

    added given code of rentals API

---
+++ vidly-mosh/9.7- Project- Build the Rentals API/after/vidly/models/rental.js
@@ -0,0 +1,75 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const { Movie } = require("./movie");
+const { Customer } = require("./customer");
+
+const Rental = mongoose.model(
+  "Rental",
+  new mongoose.Schema({
+    customer: {
+      type: new mongoose.Schema({
+        name: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+        isGold: {
+          type: Boolean,
+          default: false,
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+      }),
+      required: true,
+    },
+    movie: {
+      type: new mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 5,
+          maxlength: 255,
+        },
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 255,
+        },
+      }),
+      required: true,
+    },
+    dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    dateReturned: {
+      type: Date,
+      default: () => Date.now() + 7 * 24 * 60 * 60 * 1000,
+    },
+    rentalFee: {
+      type: Number,
+      min: 0,
+    },
+  })
+);
+
+
+function validateRental(rental) {
+  const schema = {
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  };
+
+  return Joi.validate(rental, schema);
+}
+
+exports.Rental = Rental;
+exports.validate = validateRental;
